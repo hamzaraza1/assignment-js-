a49e005e717723044a1656de8abff8ff39688c7f
hamzaraza1 <134017493+hamzaraza1@users.noreply.github.com>
2023-05-19 19:23:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,384 @@
+// CHAPTER 1
+// // alert("Hamza");
+// alert("Raza");
+// alert("hrazaofficial@gmail.com");
+// alert("Password");
+// alert("03162081361");
+// alert("You're learning JavaScript!");
+// alert("We are learning JavaScript");
+
+// CHAPTER 2
+
+// var lastName;
+// var password;
+// password = ("i dont remember");
+// var teamName = ("Pakistan");
+// alert(teamName);
+// var bestMan = "Charlie";
+// var bestMan = "John"
+// alert(bestMan);
+
+// CHAPTER 3
+// var caseQty;
+// caseQty = 144;
+// var num = 9;
+// var sum;
+// sum = 2 + 7;
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert(orderTotal);
+
+// var num = 6;
+// var add = num + 3;
+// alert(add);
+
+// CHAPTER 4
+// var productCost = 3.45;
+// var NameofBand;
+
+// var $num = 9;
+// var hamzaRaza;
+
+// var num; legal
+// var num1; legal
+// var $num legal;
+// var 1num; ilegal
+
+// CHAPTER 5
+// var num = 20 / 6;
+// alert(num)
+
+// var num1 = 20 % 6;
+// alert(num1);
+
+// var largeNum = 1000 * 2000;
+// alert(largeNum)
+
+// var num2 = 12;
+// var num3 = num2 - 3;
+// alert(num3);
+
+// var num2 = 12;
+// var num3 = num2 / 3;
+// alert(num3);
+
+// var num2 = 12;
+// var num3 = num2 * 3;
+// alert(num3);
+
+// CHAPTER 6
+
+// x = x++;
+
+// var x = 100;
+// x = --x;
+// alert(x);
+
+// var x = 50;
+// var y = x++;
+// alert(y)
+// value is 50
+
+// var y = 50;
+// var z = --y;
+// alert(z)
+// value is 49
+
+// var num = 10;
+// var newNum = --num;
+// alert(newNum)
+
+// var num = 10;
+// var newNum = ++num;
+// alert(newNum)
+
+// var num = 10;
+// var num = ++num;
+// alert(num)
+
+// CHAPTER 7
+
+// var calculatedNum = 2 + (2 * 6);
+// alert(calculatedNum)
+// value is 14
+
+// var calculatedNum = (2 + 2) * 6;
+// alert(calculatedNum)
+// value is 24
+
+// calculatedNum = (2 + 2) * (4 + 2);
+// alert(calculatedNum)
+// value is 24
+
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// alert(calculatedNum)
+// value is 18
+
+
+// var num = 2 + 2 * 4 + 10;
+// alert(num)
+// value is 20
+
+// var num = 4 / (2 * 4);
+// alert(num)
+// value is 0.5
+
+// Chapter 8 
+
+// var num = "2" + "2";
+// alert(num)
+// value is 22
+
+// var message = ("Hello," + "Dolly");
+// alert(message)
+
+// alert("33" + 3);
+
+// var country = ("Pakistan" + " " + "Zindabad");
+// alert(country)
+
+// var a = "Badri" + 313;
+// alert(a)
+
+// var firstName = "Hamza";
+// var lastName = "Raza";
+// var b = firstName + lastName;
+// alert(b)
+
+// Chapter 9
+
+// var firstName = prompt("Enter first name");
+// alert(firstName);
+
+// var country = prompt("country","China");
+// alert(country);
+
+// var yourName = prompt("Enter Your Name");
+
+// var a = prompt("You are learning Prompt","chapter 9");
+// alert(a);
+
+// var a = prompt("Your name");
+// var b = "Hamza";
+// var c = a + b;
+// alert(c);
+
+// Chapter 10 
+
+// var city = "Karachi"
+// if (city = "Karachi") {
+// console.log("The City OF Lights")
+// }
+
+// var code = 10010;
+// if(code === 10010){
+//     alert("Karachi")
+// }else{
+//     alert("Please write correct city")
+// }
+
+// var x = 1;
+// if(x === 1){
+//     x = 10;
+// }
+// console.log(x)
+
+// chapter 11
+
+// var x = 9;
+// var b = 10 
+// if(x !== b){
+//     alert("wrong")
+// }
+
+// var a = 10;
+// var b = 8;
+// if(a >= b){
+//     alert("correct")
+// }
+
+// var x = 9;
+// if(x < 10 ){
+//     alert(x = 12)
+// }
+
+// var a = 10;
+// if(a!== "10"){
+//     alert("congratulations")
+// } 
+
+// var enterName = prompt("Enter yout first name");
+// var b = "Hamza"
+// if(b === "Hamza"){
+//     alert("No match");
+// }
+
+
+// Chapter 12
+
+// var a = 10;
+// var b = 8;
+// if(a >= b){
+//     alert("correct")
+// }
+// else{
+//     alert("wrong")
+// }
+
+// var marks = +prompt("Enter marks");
+// if( marks > 80 && marks < 100){
+//     alert("A+")
+// }
+// else if(marks >= 80 && marks < 90){
+//     alert("A");
+// }
+// else if(marks >= 70 && marks < 80){
+//     alert("B");
+// }
+// else if(marks >= 60 && marks < 70){
+//     alert("c");
+// }
+// else if(marks >= 50 && marks < 60){
+//     alert("D");
+// }
+// else if(marks >= 40 && marks < 50){
+//     alert("C");
+// }
+// else if(marks >= 30 && marks < 40){
+//     alert("You have failed");
+// }
+// else{
+//     alert("Enter right Grade")
+// }
+
+
+// var a = "10";
+// if (a === 10) {
+//     alert("a is 10");
+// }else if(a !== 10){
+//     alert("a isn't equal to 10")
+// }
+   
+// var city = prompt("Enter City")
+// if(city == "karachi"){
+//     alert("It's Karachi")
+// }else if(city == "lahore"){
+//     alert("It's Lahore");
+// }
+// else{
+//     alert("Wrong City")
+// }
+
+// Chapter 13
+
+// var a = 10;
+// var b = 10;
+// var c = 5;
+// var d = 5;
+// if(a == b && c == d){
+//     alert("Correct")
+// }else{
+//     alert("Incorrect")
+// }
+
+// var a = 10;
+// var b = 10;
+// var c = 15;
+// var d = 5;
+// if(a == b || c == d){
+//     alert("Correct")
+// }else{
+//     alert("Incorrect")
+// }
+
+// var name1 = "hamza";
+// var age = 60;
+// if(name1 === "hamza" || name1 === "arsalan" && age <= 60 ){
+//     alert("You enter right details")
+// }else{
+//     alert("You enter wrong details")
+// }
+
+// var num1 = 30;
+// var num2 = 40;
+// if(num1 < num2 || num1 > num2){
+//     alert("Number 2 is greater than")
+// }else{
+//     alert("wrong")
+// }
+
+// var a = prompt("Enter your First Name");
+// var b = prompt("Enter your Last Name");
+// var firstName = "hamza";
+// var lastName = "raza";
+// if(a == firstName && b == lastName){
+//     alert("you are come in")
+// }else{
+//     alert("you cannot come in")
+// }
+
+
+// Chapter 15
+
+// var arr = [];
+// console.log(arr)
+
+// var arr = ["Hamza"];
+// console.log(arr);
+
+// var alphabet = ["h","i","j","k"];
+// alert(alphabet[2]);
+
+// var alphabet=["h","i","j","k","i", "m","n","o"];
+// console.log(alphabet.length)
+
+// chapter 16
+
+// var arr = ["Hamza"];
+// arr.push("Raza");
+// alert(arr[1])
+
+// var Alphabet=["h","i","j","k"];
+// Alphabet.pop(3);
+// console.log(Alphabet)
+
+// var Alphabet=["h","i","j","k"];
+// Alphabet.push(10);
+// console.log(Alphabet)
+
+// chapter 16 |||
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.shift(0);
+// console.log(sizes)
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.unshift(1, 2, 3);
+// console.log(sizes)
+
+// var arr = ["Hamza"];
+// arr.unshift("Raza");
+// console.log(arr)
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.splice(2,0, "L");
+// console.log(sizes)
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// var regSizes = sizes.slice(0,3);
+// console.log(regSizes);
+
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// pets.splice(1,3, "goat","cow","elephant");
+// console.log(pets)
+
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// pets.splice(1,2);
+// console.log(pets);
+
+// var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+// var redPets = pets.slice(3,5);
+// console.log(pets)
+// console.log(redPets);
+
